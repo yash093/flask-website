9d03c09d2b97372cc629fd2e7a4117c6af13854f
yash093 <yashaswar999@gmail.com>
2024-05-05 17:28:25 +0530

    added more functionalities

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from os import path
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
@@ -26,10 +27,17 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
-
